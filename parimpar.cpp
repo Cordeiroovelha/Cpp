07b0807965202bca52753b01328809fc1ec0cd85
Murilo Juttel Cordeiro <71266770+Cordeiroovelha@users.noreply.github.com>
2025-08-27 19:36:27 -0300

    parimpar.cpp

--- parimpar.cpp
+++ parimpar.cpp
@@ -9,24 +9,19 @@ using std::cin;
 using std::endl;
 
 int main(void) {
-    // declaração das variáveis
+    
     int32_t n;
-
-    // entrada de dados
+    
     cout << "Digite um numero: "; cin >> n;
     cin.ignore(80, '\n');
-
-    // processamento
     
-    if (int32_t r = n % 2; r == 0){
+    if (int32_t r = n % 2; r == 0)
         cout << n << " e par." << endl;
-    }
-    else {
+    else
         cout << n << " e impar." << endl;
-    }
 
-    // saída de dados
     cout << "Aperte <enter> para encerrar ";
     cin.get();
     return 0;
 }
+
