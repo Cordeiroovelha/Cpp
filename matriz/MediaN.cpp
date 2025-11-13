795bcf031d68ac5a36b23219a07634862f47de55
Murilo Juttel Cordeiro <cordeiro.juttel@gmail.com>
2025-11-12 22:13:14 -0300

    Refactor Media calculation and structure usage

--- matriz/MediaN.cpp
+++ matriz/MediaN.cpp
@@ -5,45 +5,59 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <iomanip>
 using namespace std;
 
-int main(void){
+struct Media{
     vector<float> array;
     size_t indice;
+    size_t frequencia;
+};
+
+
+void MediaComposta(void) {
+
+}
+
+
+int main(void){
     char resp = 's';
+    Media Dados;
+
+    cout << setprecision(2) << fixed << right;
 
     do {
         cout << "Quantos numeros na cadeia? ";
-        cin >> indice;
+        cin >> Dados.indice;
         cin.ignore(80, '\n');
-        array.resize(indice);
+        Dados.array.resize(Dados.indice);
         cout << endl;
-        for (size_t i = 0; i < indice; i++){
+        for (size_t i = 0; i < Dados.indice; i++){
             cout << "Entre o " << i+1 << "o valor: ";
-            cin >> array[i];
+            cin >> Dados.array[i];
             cin.ignore(80, '\n');
         }
 
         float soma = 0;
 
-        for (size_t i = 0; i < indice; i++){
-            soma = soma + array[i];
+        for (size_t i = 0; i < Dados.indice; i++){
+            soma = soma + Dados.array[i];
         }
 
-        // float mediana = 0;
+        float mediana = 0;
 
-        // if (indice % 2 == 0){
-	    //     float m1 = array[indice / 2 -1];
-        //     float m2 = array[indice / 2];
-        //     mediana  = array[(m1 + m2) / 2];
-	    // }
-        // else
-        //     mediana = array[indice / 2];
+        if (Dados.indice % 2 == 0){
+            int32_t meio1 = Dados.indice / 2 - 1;
+            int32_t meio2 = Dados.indice / 2;
+            mediana  = (Dados.array[meio1] + Dados.array[meio2]) / 2;
+	    }
+        else
+            mediana = Dados.array[Dados.indice / 2];
 
         cout << endl;
         cout << "soma: " << soma << endl;
-        cout << "Media: " << soma/indice << endl;
-        // cout << "Mediana: " << mediana 	<< endl;
+        cout << "Media: " << setw(5) << soma/Dados.indice << endl;
+        cout << "Mediana: " << mediana 	<< endl;
         cout << string(80, '-') << endl;
         cout << "Deseja continuar? ";
         cin.get(resp);
