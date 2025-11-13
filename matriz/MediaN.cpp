0027470d507923e88a305f28aa59e9f5f67cc625
Murilo Jutte Cordeiro <cordeiro.juttel@gmail.com>
2025-11-13 00:52:35 -0300

    Enhance MediaN program with additional functions and input handling

--- matriz/MediaN.cpp
+++ matriz/MediaN.cpp
@@ -8,20 +8,35 @@
 #include <iomanip>
 using namespace std;
 
+
+
 struct Media{
     vector<float> array;
     size_t indice;
     size_t frequencia;
 };
 
+void Pausa(void){
+    cout << "Aperte <enter> para continuar";
+    cin.get();
+    cin.ignore(80, '\n');
+    cout << endl;
+}
 
-void MediaComposta(void) {
+void MediaSimples(size_t indice, vector<float> array){
+    for (size_t i = 0; i < indice; i++){
+        
+    }
+}
+
+void MediaComposta(size_t indice, vector<float> array) {
 
 }
 
 
 int main(void){
-    char resp = 's';
+    char resp;
+    int8_t tipo;
     Media Dados;
 
     cout << setprecision(2) << fixed << right;
@@ -32,20 +47,35 @@ int main(void){
         cin.ignore(80, '\n');
         Dados.array.resize(Dados.indice);
         cout << endl;
+
+        cout << "Cadeia Simple[1] ou Composta[2]? ";
+        cin >> tipo;
+
+        if (tipo == 1)
+            MediaSimples(Dados.indice, Dados.array);
+        else if (tipo == 2)
+            MediaComposta(Dados.indice, Dados.array);
+        else{
+            cout << "Tipo de Conta não existente";
+            Pausa();
+        }
+            
+
         for (size_t i = 0; i < Dados.indice; i++){
             cout << "Entre o " << i+1 << "o valor: ";
             cin >> Dados.array[i];
             cin.ignore(80, '\n');
         }
 
-        float soma = 0;
 
+        // calculo da soma e media
+        float soma = 0;
         for (size_t i = 0; i < Dados.indice; i++){
             soma = soma + Dados.array[i];
         }
 
+        // calculo de mediana
         float mediana = 0;
-
         if (Dados.indice % 2 == 0){
             int32_t meio1 = Dados.indice / 2 - 1;
             int32_t meio2 = Dados.indice / 2;
