0ce9ba8ce275788d57f005b5f95b3ae87d36dc51
Cordeiroovelha <cordeiro.juttel@gmail.com>
2025-11-27 21:48:56 -0300

    add estrela.cpp

---
+++ Laço/Estrela.cpp
@@ -0,0 +1,18 @@
+// estrela.cpp
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int32_t b;
+    cout << "Qual o tamanho da base? ";
+    cin >> b;
+    cin.ignore(80, '\n');
+
+    for (int32_t i = 0; i <= b; i++){
+        for (int32_t j = 0; j <= i; j++)
+            cout << "*";
+        cout << endl;
+    }
+    return 0;
+}
