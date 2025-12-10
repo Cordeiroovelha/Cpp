1b93b950615e129f5bc04139ffe37cc86b9f513a
CordeiroOvelha <cordeiro.juttel@gmail.com>
2025-12-10 13:34:57 -0300

    a

--- Orientado ao Objeto/EntraNota.cpp
+++ Orientado ao Objeto/EntraNota.cpp
@@ -6,21 +6,12 @@
 #include <iomanip>
 #include <vector>
 #include <print>
+#include <string>
 using namespace std;
 
-class Cliente {
+class Aluno{
     private:
-        uint64_t mesa;
-        vector<int32_t> menu;
-        float total;
-    public:
-        Cliente : menu(4) {}
-        Cliente(uint64_t mesa, int32_t menu, float total) 
-        : mesa(mesa), menu(menu), total(total) {}
-
-        void Saida(void){
-            for (size_t i = 0; i <= menu.size; i++)
-        }
-    
-        ~Cliente() {}
-};
+        string Nome;
+        int32_t Classe
+        vector<float> Nota;
+}
