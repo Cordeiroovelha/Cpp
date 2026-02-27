ffd08159543068c9a1cf1dec252842eae1e520de
Murilo Jutte Cordeiro <cordeiro.juttel@gmail.com>
2026-02-27 00:44:48 -0300

    correção de ListaEncadeada.cpp

--- matriz/ListaEncadeada.cpp
+++ matriz/ListaEncadeada.cpp
@@ -2,52 +2,52 @@
 
 struct knot
 {
-	int date;
-	knot* next;
+    int date;
+    knot* next;
 };
 
 
 void InsertInEnd(knot*& head, int date){
-	knot* newKnot = new knot{date, nullptr};
-	if(head == nullptr)
-		head = newKnot;
-	else{
-		knot* aux = head;
-		while(aux->next != nullptr)
-			aux = aux->next;
-		aux->next = newKnot;
-	}
+    knot* newKnot = new knot{date, nullptr};
+    if(head == nullptr)
+        head = newKnot;
+    else{
+        knot* aux = head;
+        while(aux->next != nullptr)
+            aux = aux->next;
+        aux->next = newKnot;
+    }
 }
-// incompleto
+
 void InsertInStart(knot*& head, int date){
-	knot* newKnot = new knot{date, head};
-	
-	if (head == nullptr){
-		head = newKnot;
-		newKnot->next = head;
-	}else{
-		head = newKnot;
-	}
+    knot* newKnot = new knot{date, head};
+    head = newKnot;
 }
 
 void ViewArray(const knot* head){
-	const knot* aux = head;
-	while(aux != nullptr){
-		std::cout << aux->date << "\t";
-		aux = aux->next;
-	}
-	std::cout << std::endl;
+    const knot* aux = head;
+    while(aux != nullptr){
+        std::cout << aux->date << "\t";
+        aux = aux->next;
+    }
+    std::cout << std::endl;
 }
 
 int main(void){
-	knot* head = nullptr;
-	int newValue;
-	do{
-		std::cin >> newValue;
-		//InsertInEnd(head, newValue);
-		InsertInStart(head, newValue);
-		ViewArray(head);
-	}while(newValue != -1);
-	
-	return 0;
+    knot* head = nullptr;
+    int newValue;
+    do{
+        std::cin >> newValue;
+        //InsertInEnd(head, newValue);
+        InsertInStart(head, newValue);
+        ViewArray(head);
+    }while(newValue != -1);
+
+    while(head != nullptr) {
+        knot* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
 }
