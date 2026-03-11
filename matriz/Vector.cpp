3503555fc1e322685eaa330d812c9ea9210f018c
Murilo Jutte Cordeiro <cordeiro.juttel@gmail.com>
2026-03-11 16:28:02 -0300

    alteraçoes no codigo vetor.cpp

--- matriz/Vector.cpp
+++ matriz/Vector.cpp
@@ -1,8 +1,8 @@
-// usos da biblioteca vector
+#include <print>
+#include <chrono>
+#include<vector>
 
-#include <vector>
-#include <iostream>
-
+using namespace std::chrono;
 
 template<typename T>
 class Vector {
@@ -10,66 +10,122 @@ public:
     ~Vector() {
         delete []m_data;
     }
-    
+
     constexpr void clear(){
         delete []m_data;
         m_data = nullptr;
         m_size = 0;
     }
-    
+
     constexpr void resize(const std::size_t sz){
-        auto newData = new T[sz];
+        auto newData = new T[sz]{};
         const auto minSize = (sz < m_size) ? sz : m_size;
-        
+
         for(std::size_t i = 0; i < minSize; i++){
             newData[i] = m_data[i];
         }
-        
-        
+
+
         delete []m_data;
         m_data = newData;
         m_size = sz;
-        
+
     }
-    
+
     constexpr void push_back(const T& value){
-        auto newData = new T[m_size + 1];
-        
-        for(std::size_t i = 0; i < m_size; i++){
-            newData[i] = m_data[i];
+        if(m_size == m_capacity){
+            m_capacity = m_size > 0? m_size*2 : 2;
+
+            auto newData = new T[m_capacity];
+
+            for(std::size_t i = 0; i < m_size; i++){
+                newData[i] = m_data[i];
+            }
+
+
+            delete []m_data;
+            m_data = newData;
         }
-        
-        newData[m_size] = value;
-        delete []m_data;
-        m_data = newData;
+
+        m_data[m_size] = value;
         ++m_size;
     }
-    
+
     [[nodiscard]] constexpr std::size_t size() const noexcept{
         return m_size;
     }
-    
+
     [[nodiscard]] constexpr bool empty() const noexcept{
         return m_size == 0;
     }
-    
+
     [[nodiscard]] constexpr T& operator[](const std::size_t index) noexcept {
         return m_data[index];
     }
-    
+
     [[nodiscard]] constexpr const T& operator[](const std::size_t index) const noexcept {
         return m_data[index];
     }
-    
+
 private:
     T *m_data{nullptr};
+    std::size_t m_capacity{0};
     std::size_t m_size{0};
 };
 
 
+template<typename VecType>
+void print(const VecType &vec){
+    for(std::size_t i = 0; i < vec.size(); ++i){
+        std::println("{}", vec[i]);
+    }
+}
+
+template<typename VecType>
+void performarceTest(const std::size_t size){
+    VecType vec;
+    const auto start = high_resolution_clock::now();
+
+    for(std::size_t i{0}; i < size; ++i)
+        vec.push_back(i);
+
+    const auto end = high_resolution_clock::now();
+    const auto time = duration_cast<milliseconds>(end - start).count();
+    std::println("Total (ms) {}", time);
+}
+
 int main()
 {
-    std::vector<int> vec;
-    std::cout << "Hello World" << std::endl;
+    std::println("Minha versão");
+    performarceTest<Vector<int>>(1000000);
+    std::println("Biblioteca std::Vector");
+    performarceTest<std::vector<int>>(1000000);
+
+    Vector<int> vec;
+
+    std::println("Size: {}", vec.size());
+    std::println("Empty: {}", vec.empty());
+
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    print(vec);
+
+    std::println("Size: {}", vec.size());
+    std::println("Empty: {}", vec.empty());
+
+    vec.resize(10);
+    print(vec);
+    std::println("Size: {}", vec.size());
+
+    vec.resize(2);
+    print(vec);
+    std::println("Size: {}", vec.size());
+
+    vec.clear();
+    std::println("Size: {}", vec.size());
+    std::println("Empty: {}", vec.empty());
+
     return 0;
 }
